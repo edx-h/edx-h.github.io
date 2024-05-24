a0e5e6ca6e83abd5a87a24bf0882fa11be8e58f6
xinjie-h <xinjie.h@outlook.com>
2024-05-25 00:24:49 +0800

    keep dev and master branch different with this sheel script

---
+++ merge_master.sh
@@ -0,0 +1,10 @@
+git switch master
+git diff master dev --name-only |
+grep -v 'Dockerfile' |
+grep -v 'Gemfile.lock' |
+grep -v '_plugins/external-posts.rb' |
+grep -v 'bin/entry_point.sh' |
+grep -v 'docker-compose.yml' |
+grep -v 'feed.xml' |
+sed 's/.*/"&"/' |
+xargs git checkout dev
