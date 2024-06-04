8033406be3601eb473092f0d58042bcf51be3b9e
xinjie-h <xinjie.h@outlook.com>
2024-06-04 18:15:39 +0800

    modify merge script

--- merge_master.sh
+++ merge_master.sh
@@ -6,5 +6,6 @@ grep -v '_plugins/external-posts.rb' |
 grep -v 'bin/entry_point.sh' |
 grep -v 'docker-compose.yml' |
 grep -v 'feed.xml' |
+grep -v 'merge_master.sh'
 sed 's/.*/"&"/' |
 xargs git checkout dev
